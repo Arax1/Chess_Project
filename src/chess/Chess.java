ee1d8921144ef640bbeec9de3d530a4cd788b1b1
Arax1 <anandraju55@hotmail.com>
2019-03-27 16:28:42 -0400

    implemented draw functionality

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -1,3 +1,10 @@
+/**
+ * 
+ * 
+ * 
+ * 
+ * */
+
 package chess;
 
 import java.util.ArrayList;
@@ -34,8 +41,11 @@ public class Chess {
 			player = turns % 2 == 0 ? "White" : "Black";
 			king = (pc == 'w')? board.black_king: board.white_king;
 
-			if(checkmate)
+			if(checkmate) {
+				draw = false;
 				break;
+			}
+				
 
 			System.out.print(player + "'s move: ");
 			str = scan.nextLine();
@@ -102,7 +112,12 @@ public class Chess {
 		
 		scan.close();
 		turns++;
-		System.out.println(player + " wins!");
+		
+		if(draw)
+			System.out.println("draw");
+		
+		else
+			System.out.println(player + " wins!");
 
 	}
 	
