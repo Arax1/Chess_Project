46b2f4b246c33ef23ba5c39f2f6c9fce3543ee6a
Anand Raju <anandraju55@hotmail.com>
2019-03-27 00:35:53 +0000

    Rook.java edited online with Bitbucket

--- src/parts/Rook.java
+++ src/parts/Rook.java
@@ -7,19 +7,11 @@ public class Rook implements Piece {
 	private int column, row;
 	private char color;
 	
-	public boolean hasmoved = false;
-	
 	public Rook(int c, int r) {
 		column = c;
 		row = r;
 		
 		color = (r == 7) ? 'b' : 'w';
-	}
-	public Rook(int c, int r, char co) {
-		column = c;
-		row = r;
-		
-		color = co;
 	}
 	
 	@Override
@@ -49,7 +41,7 @@ public class Rook implements Piece {
 		if(c == column) {
 			int direction = (r > row) ? 1 : -1;
 			for(int x = row + direction; x != r; x += direction)
-				if(b.filled(c,x))
+				if(b.board[c][x].filled)
 					return false;
 			
 			return true;
@@ -58,7 +50,7 @@ public class Rook implements Piece {
 		if(r == row) {
 			int direction = (c > column) ? 1 : -1;
 			for(int x = column + direction; x != c; x+= direction)
-				if(b.filled(c, x))
+				if(b.board[x][r].filled)
 					return false;
 			
 			return true;
@@ -72,25 +64,55 @@ public class Rook implements Piece {
 		if(!threatens(c,r,b))
 			return false;
 		
-		if(b.filled(c,r))
+		if(b.board[c][r].filled)
 			if(b.board[c][r].p.getColor() == color)
 				return false;
 		
-		column = c;
-		row = r;
 		b.en_passant = null;
-		hasmoved = true;
 		return true;
+	}
+	
+	public boolean canBlockPiece(Piece threat, Piece victim, Board b) {
+		
+		int o_row = row;
+		int o_col = column;
+		Piece o_p = b.en_passant;
+		
+		ArrayList<Square> threat_spots = threat.getAllMoves(b);
+		
+		for(Square s: threat_spots) {
+			
+			if(moveTo(s.column, s.row, b)) {
+				
+				b.movePiece(row, column, s.column, s.row);
+				
+				if(!threat.threatens(victim.getColumn(), victim.getRow(), b)) {
+					return true;
+				}
+					
+				else {
+					
+					row = o_row;
+					column = o_col;
+					b.en_passant = o_p;
+				}
+			}
+				
+		}
+		
+		return false;
 	}
 
 	public char getColor() {
 		return color;
 	}
+	
 	@Override
 	public int getRow() {
 		// TODO Auto-generated method stub
 		return row;
 	}
+
 	@Override
 	public int getColumn() {
 		// TODO Auto-generated method stub
@@ -101,24 +123,35 @@ public class Rook implements Piece {
 		return getColor() + "R";
 	}
 
-
 	@Override
-	public ArrayList<Square> getAllMoves(Board board) {
+	public ArrayList<Square> getAllMoves(Board b) {
 		// TODO Auto-generated method stub
-		
 		ArrayList<Square> moves = new ArrayList<Square>();
+		Piece o_pas = b.en_passant;
 		
 		for(int col = 0; col < 8; col++) {
 			
-			for(int ro = 0; ro < 8; ro++){
+			for(int ro = 0; ro < 8; ro++) {
 				
-				if(threatens(col, ro, board))
-					moves.add(board.board[col][ro]);
-					
+				if(moveTo(col, ro, b)) {
+					b.en_passant = o_pas;
+					moves.add(b.board[col][ro]);
+				}		
 			}
 		}
-			
 		
 		return moves;
+	}
+	
+	@Override
+	public void setRow(int r) {
+		// TODO Auto-generated method stub
+		row = r;
+	}
+
+	@Override
+	public void setColumn(int c) {
+		// TODO Auto-generated method stub
+		column = c;
 	}
 }
